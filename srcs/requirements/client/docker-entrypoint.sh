27fcb2a128a8931bc7b1f4c9e6c8ec9684483d13
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2025-06-18 08:37:46 +0200

    log sizes limited to 3megas and 3 rotations

--- srcs/requirements/client/docker-entrypoint.sh
+++ srcs/requirements/client/docker-entrypoint.sh
@@ -1,3 +1,15 @@
 #!/bin/sh
-echo "Container started!"
-exec /bin/sh
+set -ex
+
+# Function to read secret from file
+read_secret() {
+    local file="$1"
+    if [ -f "$file" ]; then
+        cat "$file"
+    fi
+}
+
+DBSERVER_MSQL_PASSWORD=$(read_secret "$DBSERVER_MSQL_PASSWORD_FILE")
+echo "msql pass:>$DBSERVER_MSQL_PASSWORD<"
+
+exec su - mysql -s /bin/sh -c "/usr/bin/mariadb -u mysql -p$DBSERVER_MSQL_PASSWORD -h 192.168.1.2 --datadir=/var/lib/mysql"
