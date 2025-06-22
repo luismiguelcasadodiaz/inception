e59ad181ac397b40416dbfdf8c4e5f6964f514a0
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2025-06-22 10:39:01 +0200

    basic php-fpm configuration

---
+++ srcs/requirements/contentserver/index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Pagina de prueba</title>
+    </head>
+    <body>
+
+        <?php
+            echo "Hello Inception World! Current timestamp: " . date("Y-m-d H:i:s T") . " (From PHP-FPM LMCD)";
+        ?>
+
+    </body>
+</html>
