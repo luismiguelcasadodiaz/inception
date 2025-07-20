ffc11b7680471a61f61522ed7e3c9a10c7e3a29a
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2025-07-20 12:12:51 +0200

    increaded verbosity in health check

--- srcs/requirements/dbserver/healthcheck.sh
+++ srcs/requirements/dbserver/healthcheck.sh
@@ -3,6 +3,7 @@ set -ex
 
 # Read the root password from the mounted secret file
 ROOT_PASSWORD=$(cat /run/secrets/dbserver_root_password)
+echo "ROOT_PASSWORD=$ROOT_PASSWORD"
 
 # Ping MariaDB using the retrieved password
 mariadb-admin ping -u root --password="$ROOT_PASSWORD" -h localhost
