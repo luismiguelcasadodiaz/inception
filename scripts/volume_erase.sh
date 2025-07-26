5f17bba12c0eddef3d372b25fdc28c709a85baa2
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2025-07-26 11:30:15 +0200

    include volumen erase

--- scripts/volume_erase.sh
+++ scripts/volume_erase.sh
@@ -1,6 +1,9 @@
 #!/bin/sh
 docker exec dbserver /bin/rm -rf /var/lib/mysql/*
 docker exec contentserver /bin/rm -rf /www/*
+docker volume rm inception_db_data
+docker volume rm inception_wp_data
+
 rm -rf /home/luicasad/data/wp/*
 rm -rf /home/luicasad/data/db/*
 rm -rf /home/luicasad/inception
