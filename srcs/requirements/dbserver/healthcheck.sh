eda588f51a2bf80947b099feea4e9a320df63d46
Luis Miguel Casado Diaz <luismiguelcasadodiaz@gmail.com>
2025-07-30 20:11:53 +0200

    Changed logic in healthcheck

--- srcs/requirements/dbserver/healthcheck.sh
+++ srcs/requirements/dbserver/healthcheck.sh
@@ -3,7 +3,7 @@ set -ex
 
 if [ ! -f /run/secrets/dbserver_root_password ]; then
   echo "Secret not found — skipping healthcheck for now"
-  exit 0
+  exit 1
 fi
 
 # Read the root password from the mounted secret file
@@ -13,5 +13,11 @@ echo "ROOT_PASSWORD=$ROOT_PASSWORD"
 # Ping MariaDB using the retrieved password
 mariadb-admin ping -u root --password="$ROOT_PASSWORD" -h localhost
 
-# If mariadb-admin ping exits with 0, the healthcheck passes
-exit 0
+# Check the exit status of the mariadb-admin ping command
+if [ $? -eq 0 ]; then
+  echo "MariaDB ping successful. Healthcheck passed."
+  exit 0 # MariaDB is healthy
+else
+  echo "MariaDB ping failed. Healthcheck failed."
+  exit 1 # MariaDB is unhealthy
+fi
